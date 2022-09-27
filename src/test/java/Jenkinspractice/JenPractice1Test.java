ec116e950b4e9d7a1ca8f213ec20279d80d367a4
Ramprasad0123 <karishettir98@gmail.com>
2022-09-28 00:36:15 +0530

    jenkinspractice1Test updated...

--- src/test/java/Jenkinspractice/JenPractice1Test.java
+++ src/test/java/Jenkinspractice/JenPractice1Test.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 public class JenPractice1Test {
 	@Test
+//testcase updated!
 	public void jenPractice1() {
 		Reporter.log("Jenkins-Practice1",true);
 		Reporter.log("Jenkins-Practice11",true);
